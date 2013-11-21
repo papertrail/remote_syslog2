f28febb1d9ff7303802865422b75bcb9a6c260c3
Paul Morton <pmorton@biaprotect.com>
2013-11-21 10:11:01 -0800

    Remove logo from syslog package.

--- syslog/syslog.go
+++ syslog/syslog.go
@@ -3,12 +3,9 @@ package syslog
 import (
 	"crypto/tls"
 	"fmt"
-	"github.com/howbazaar/loggo"
 	"github.com/sevenscale/remote_syslog2/syslog/certs"
 	"net"
 )
-
-var log = loggo.GetLogger("syslog")
 
 type Conn struct {
 	hostname string
