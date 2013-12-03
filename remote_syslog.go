52b159c71e8cd6fa7f1d093eb3340bc5986c84e3
Paul Morton <pmorton@biaprotect.com>
2013-12-03 15:11:38 -0800

    Move the configuration manager into it's own file

--- remote_syslog.go
+++ remote_syslog.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/ActiveState/tail"
 	"github.com/howbazaar/loggo"
-	"github.com/sevenscale/remote_syslog2/papertrail"
 	"github.com/sevenscale/remote_syslog2/syslog"
-	"github.com/sevenscale/remote_syslog2/syslog/certs"
-	"io/ioutil"
-	"launchpad.net/goyaml"
 	"os"
 	"path"
 	"time"
@@ -43,139 +38,6 @@ func tailFile(file string, logger *syslog.Conn) {
 	}
 
 	log.Errorf("Tail worker executed abnormally")
-}
-
-type ConfigFile struct {
-	Files       []string
-	Destination struct {
-		Host     string
-		Port     int
-		Protocol string
-	}
-	Hostname string
-	CABundle string `yaml:"ca_bundle"`
-}
-
-type ConfigManager struct {
-	Config ConfigFile
-	Flags  struct {
-		Hostname   string
-		ConfigFile string
-		LogLevels  string
-	}
-	CertBundle certs.CertBundle
-}
-
-func NewConfigManager() ConfigManager {
-	cm := ConfigManager{}
-	err := cm.Initialize()
-
-	if err != nil {
-		log.Criticalf("Failed to configure the application: %s", err)
-		os.Exit(1)
-	}
-
-	return cm
-}
-
-func (cm *ConfigManager) Initialize() error {
-	cm.parseFlags()
-
-	err := cm.loadConfigFile()
-	if err != nil {
-		return err
-	}
-
-	err = cm.loadCABundle()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (cm *ConfigManager) parseFlags() {
-	flag.StringVar(&cm.Flags.ConfigFile, "config", "/etc/remote_syslog2/config.yaml", "the configuration file")
-	flag.StringVar(&cm.Flags.Hostname, "hostname", "", "the name of this host")
-	flag.StringVar(&cm.Flags.LogLevels, "log", "<root>=INFO", "\"logging configuration <root>=INFO;first=TRACE\"")
-	flag.Parse()
-}
-
-func (cm *ConfigManager) loadConfigFile() error {
-	log.Infof("Reading configuration file %s", cm.Flags.ConfigFile)
-
-	file, err := ioutil.ReadFile(cm.Flags.ConfigFile)
-	if err != nil {
-		return fmt.Errorf("Could not read the config file: %s", err)
-	}
-
-	err = goyaml.Unmarshal(file, &cm.Config)
-	if err != nil {
-		return fmt.Errorf("Could not parse the config file: %s", err)
-	}
-	return nil
-}
-
-func (cm *ConfigManager) loadCABundle() error {
-	bundle := certs.NewCertBundle()
-	if cm.Config.CABundle == "" {
-		log.Infof("Loading default certificates")
-
-		loaded, err := bundle.LoadDefaultBundle()
-		if loaded != "" {
-			log.Infof("Loaded certificates from %s", loaded)
-		}
-		if err != nil {
-			return err
-		}
-
-		log.Infof("Loading papertrail certificates")
-		err = bundle.ImportBytes(papertrail.BundleCert())
-		if err != nil {
-			return err
-		}
-
-	} else {
-		log.Infof("Loading certificates from %s", cm.Config.CABundle)
-		err := bundle.ImportFromFile(cm.Config.CABundle)
-		if err != nil {
-			return err
-		}
-
-	}
-	cm.CertBundle = bundle
-	return nil
-}
-
-func (cm *ConfigManager) Hostname() string {
-	switch {
-	case cm.Flags.Hostname != "":
-		return cm.Flags.Hostname
-	case cm.Config.Hostname != "":
-		return cm.Config.Hostname
-	default:
-		hostname, _ := os.Hostname()
-		return hostname
-	}
-}
-
-func (cm *ConfigManager) DestHost() string {
-	return cm.Config.Destination.Host
-}
-
-func (cm ConfigManager) DestPort() int {
-	return cm.Config.Destination.Port
-}
-
-func (cm *ConfigManager) DestProtocol() string {
-	return cm.Config.Destination.Protocol
-}
-
-func (cm *ConfigManager) Files() []string {
-	return cm.Config.Files
-}
-
-func (cm *ConfigManager) LogLevels() string {
-	return cm.Flags.LogLevels
 }
 
 func main() {
