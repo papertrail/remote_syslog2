dbf825e43ae158cc985b3accdd0b99dce126fed8
Paul Morton <pmorton@biaprotect.com>
2013-12-03 15:33:12 -0800

    Simplify the worker registry using a map!

--- worker_registry.go
+++ worker_registry.go
@@ -1,70 +1,36 @@
 package main
 
 import (
-	"container/list"
-	"fmt"
 	"sync"
 )
 
 func NewWorkerRegistry() WorkerRegistry {
-	return WorkerRegistry{workers: list.New()}
+	return WorkerRegistry{workers: make(map[string]bool)}
 }
 
 type WorkerRegistry struct {
-	workers *list.List
+	workers map[string]bool
 	mu      sync.RWMutex
 }
 
 func (w *WorkerRegistry) Exists(worker string) bool {
-	_, err := w.Find(worker)
-	if err != nil {
-		return false
-	}
-	return true
-}
-
-func (w *WorkerRegistry) Find(worker string) (*list.Element, error) {
 	w.mu.RLock()
 	defer w.mu.RUnlock()
-	log.Tracef("Called thread safe Find")
-	log.Tracef("Looking for %s in the worker registry", worker)
-	element, err := w.find(worker)
-	if err != nil {
-		log.Tracef("%s was not found in the worker registry", worker)
-		return nil, err
-	}
-	log.Tracef("%s was found in the worker registry", worker)
-	return element, nil
-}
-
-func (w *WorkerRegistry) find(worker string) (*list.Element, error) {
-	log.Tracef("Called non-thread safe find")
-	for e := w.workers.Front(); e != nil; e = e.Next() {
-		log.Tracef("Checking if %s == %s", worker, e.Value)
-		if e.Value == worker {
-			return e, nil
-		}
-	}
-	return nil, fmt.Errorf("Failed to find worker %s", worker)
+	_, ok := w.workers[worker]
+	return ok
 }
 
 func (w *WorkerRegistry) Add(worker string) {
 	w.mu.Lock()
 	defer w.mu.Unlock()
 	log.Tracef("Adding %s to worker registry", worker)
-	w.workers.PushBack(worker)
+	w.workers[worker] = true
 }
 
 func (w *WorkerRegistry) Remove(worker string) error {
 	w.mu.Lock()
 	defer w.mu.Unlock()
 	log.Tracef("Removing %s from worker registry", worker)
-	workerElement, err := w.find(worker)
-	if err != nil {
-		log.Tracef("Failed to remove worker: %s", err)
-		return err
-	}
-	w.workers.Remove(workerElement)
-	log.Tracef("Removed %s from worker registry", worker)
+	delete(w.workers, worker)
 	return nil
 }
