e9bfd695dda189f82bf0689bbab7a358006790df
Paul Morton <pmorton@biaprotect.com>
2014-01-08 18:31:35 -0800

    Clarify test message

--- remote_syslog_test.go
+++ remote_syslog_test.go
@@ -39,6 +39,6 @@ func TestFilters(t *testing.T) {
 
 	message = "0000"
 	if !matchExps(message, expressions) {
-		t.Errorf("Did not expect \"%s\" to match \"%s\"", message, expressions[0])
+		t.Errorf("Expected \"%s\" to match \"%s\"", message, expressions[0])
 	}
 }
