d2489a96a12be3566865ab0911a258c8148bf985
Troy Davis <troy@yort.com>
2014-01-15 04:54:48 -0800

    Tweak build script

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+BUILDPATH="build/remote_syslog2"
+
 set -e
 
-mkdir -p build/remote_syslog2
+mkdir -p $BUILDPATH
 
-godep go build -o build/remote_syslog2/remote_syslog2 .
-cp README.md LICENSE example_config.json build/remote_syslog2
+godep go build -o $BUILDPATH/remote_syslog2 .
+cp README.md LICENSE example_config.yaml $BUILDPATH
 
-cd pkg
+cd $BUILDPATH/..
 rm -f remote_syslog2.tar.gz
-tar -czf remote_syslog2.tar.gz remote_syslog2
+tar -czf remote_syslog2.tar.gz `basename $BUILDPATH`
 rm -r remote_syslog2
