cdcc0381de74384bca911deda65b6c70a0dcf4d8
Joel Reymont <joelr1@gmail.com>
2015-06-29 18:14:37 +0200

    go back to a 10s default

--- config_manager.go
+++ config_manager.go
@@ -17,7 +17,7 @@ import (
 )
 
 const (
-	MinimumRefreshInterval = RefreshInterval(1 * time.Second)
+	MinimumRefreshInterval = RefreshInterval(10 * time.Second)
 	DefaultConfigFile      = "/etc/log_files.yml"
 )
 
