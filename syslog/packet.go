486dc416cb84507cd17108f47fa61528a8c2b65e
Joel Reymont <joelr1@gmail.com>
2015-06-30 13:19:35 +0200

    gocheck and references

--- syslog/packet.go
+++ syslog/packet.go
@@ -20,18 +20,18 @@ type Packet struct {
 const rfc5424time = "2006-01-02T15:04:05.999999Z07:00"
 
 // The combined Facility and Severity of this packet. See RFC5424 for details.
-func (p Packet) Priority() Priority {
+func (p *Packet) Priority() Priority {
 	return (p.Facility << 3) | p.Severity
 }
 
-func (p Packet) cleanMessage() string {
+func (p *Packet) cleanMessage() string {
 	s := strings.Replace(p.Message, "\n", " ", -1)
 	s = strings.Replace(s, "\r", " ", -1)
 	return strings.Replace(s, "\x00", " ", -1)
 }
 
 // Generate creates a RFC5424 syslog format string for this packet.
-func (p Packet) Generate(max_size int) string {
+func (p *Packet) Generate(max_size int) string {
 	ts := p.Time.Format(rfc5424time)
 	if max_size == 0 {
 		return fmt.Sprintf("<%d>1 %s %s %s - - - %s", p.Priority(), ts, p.Hostname, p.Tag, p.cleanMessage())
