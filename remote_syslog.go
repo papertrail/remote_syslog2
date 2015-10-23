1be8b831e5cd3e4600c5ace8c4b8ca1ce6ce58f8
Leon Sodhi <leon@papertrailapp.com>
2015-10-23 21:37:37 +0100

    Continually retries the first connection rather than exiting on failure

--- remote_syslog.go
+++ remote_syslog.go
@@ -110,18 +110,24 @@ func main() {
 
 	loggo.ConfigureLoggers(cm.LogLevels())
 
-	raddr := net.JoinHostPort(cm.DestHost(), strconv.Itoa(cm.DestPort()))
-	log.Infof("Connecting to %s over %s", raddr, cm.DestProtocol())
-	logger, err := syslog.Dial(cm.Hostname(), cm.DestProtocol(), raddr, cm.RootCAs())
+	raddr := net.JoinHostPort(cm.DestHost(), strconv.Itoa(cm.DestPort()))	
+	var logger *syslog.Logger	
+	for {
+		var err error
+		log.Infof("Connecting to %s over %s", raddr, cm.DestProtocol())
+		logger, err = syslog.Dial(cm.Hostname(), cm.DestProtocol(), raddr, cm.RootCAs())
 
-	if err != nil {
-		log.Criticalf("Cannot connect to server: %v", err)
-		os.Exit(1)
+		if err == nil {
+			break
+		} else {
+			log.Errorf("Cannot connect to server: %v", err)
+			time.Sleep(10 * time.Second)
+		}
 	}
 
 	go tailFiles(cm.Files(), cm.ExcludeFiles(), cm.ExcludePatterns(), cm.RefreshInterval(), logger, cm.Severity(), cm.Facility(), cm.Poll())
 
-	for err = range logger.Errors {
+	for err := range logger.Errors {
 		log.Errorf("Syslog error: %v", err)
 	}
 }
