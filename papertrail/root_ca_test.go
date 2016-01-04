de03e44d6487dbbc114fb82e7ae5adc786e45d2e
Leon Sodhi <leon@papertrailapp.com>
2016-01-04 21:15:28 +0000

    Updated cert to handle logs.pt changes

--- papertrail/root_ca_test.go
+++ papertrail/root_ca_test.go
@@ -9,7 +9,7 @@ import (
 func TestCerts(t *testing.T) {
 	h := md5.New()
 	h.Write(certs())
-	expected := "cee9b8d2d503188ccecbb22b49cd3bec"
+	expected := "3203239f3684b19dc0aec8554a2613a7"
 	actual := fmt.Sprintf("%x", h.Sum(nil))
 	if actual != expected {
 		t.Errorf("Mismatched hash for papertrail certs, expected %s got %s", expected, actual)
@@ -18,7 +18,9 @@ func TestCerts(t *testing.T) {
 
 func TestRootCA(t *testing.T) {
 	pool := RootCA()
-	if len(pool.Subjects()) != 3 {
-		t.Errorf("error loading RootCA")
+	expected := 154
+	actual := len(pool.Subjects())
+	if actual != expected {
+		t.Errorf("Error loading RootCA, expected %d subjects got %d", expected, actual)
 	}
 }
