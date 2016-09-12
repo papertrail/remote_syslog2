df933042762321b9c56250ef7bad250db408ea1c
mark.martin <mark.martin@papertrail.com>
2016-09-12 14:46:56 -0700

    config cleanup

--- config.go
+++ config.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"crypto/x509"
+	"fmt"
 	"os"
 	"path/filepath"
+	"reflect"
+	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
+	"github.com/mitchellh/mapstructure"
 	"github.com/papertrail/remote_syslog2/papertrail"
+	"github.com/papertrail/remote_syslog2/syslog"
 	"github.com/papertrail/remote_syslog2/utils"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
@@ -16,8 +22,6 @@ import (
 var (
 	config  *viper.Viper
 	Version string
-
-	MinimumRefreshInterval = time.Second
 )
 
 const (
@@ -27,29 +31,29 @@ const (
 // The global Config object for remote_syslog2 server. "mapstructure" tags
 // signify the config file key names.
 type Config struct {
-	ConfigFile           string   `mapstructure:"config_file"`
-	ConnectTimeout       int      `mapstructure:"connect_timeout"`
-	WriteTimeout         int      `mapstructure:"write_timeout"`
-	NewFileCheckInterval int      `mapstructure:"new_file_check_interval"`
-	ExcludeFiles         []string `mapstructure:"exclude_files"`
-	ExcludePatterns      []string `mapstructure:"exclude_patterns"`
-	LogLevels            string   `mapstructure:"log_levels"`
-	DebugLogFile         string   `mapstructure:"debug_log_file"`
-	PidFile              string   `mapstructure:"pid_file"`
-	TcpMaxLineLength     int      `mapstructure:"tcp_max_line_length"`
-	NoDaemonize          bool     `mapstructure:"no_daemonize"`
-	TCP                  bool     `mapstructure:"tcp"`
-	TLS                  bool     `mapstructure:"tls"`
-	Files                []interface{}
+	ConnectTimeout       time.Duration    `mapstructure:"connect_timeout"`
+	WriteTimeout         time.Duration    `mapstructure:"write_timeout"`
+	NewFileCheckInterval time.Duration    `mapstructure:"new_file_check_interval"`
+	ExcludeFiles         []*regexp.Regexp `mapstructure:"exclude_files"`
+	ExcludePatterns      []*regexp.Regexp `mapstructure:"exclude_patterns"`
+	LogLevels            string           `mapstructure:"log_levels"`
+	DebugLogFile         string           `mapstructure:"debug_log_file"`
+	PidFile              string           `mapstructure:"pid_file"`
+	TcpMaxLineLength     int              `mapstructure:"tcp_max_line_length"`
+	NoDaemonize          bool             `mapstructure:"no_detach"`
+	TCP                  bool             `mapstructure:"tcp"`
+	TLS                  bool             `mapstructure:"tls"`
+	Files                []LogFile
 	Hostname             string
-	Severity             string
-	Facility             string
+	Severity             syslog.Priority
+	Facility             syslog.Priority
 	Poll                 bool
 	Destination          struct {
 		Host     string
 		Port     int
 		Protocol string
 	}
+	RootCAs *x509.CertPool
 }
 
 type LogFile struct {
@@ -65,6 +69,8 @@ func init() {
 	config.SetDefault("destination.protocol", "udp")
 	config.SetDefault("tcp_max_line_length", 99990)
 	config.SetDefault("debug_log_file", "/dev/null")
+	config.SetDefault("connect_timeout", 30*time.Second)
+	config.SetDefault("write_timeout", 30*time.Second)
 
 	// set available commandline flags here:
 	pflag.StringP("configfile", "c", "/etc/log_files.yml", "Path to config")
@@ -110,7 +116,7 @@ func init() {
 	// only present this flag to systems that can daemonize
 	if utils.CanDaemonize {
 		pflag.BoolP("no-detach", "D", false, "Don't daemonize and detach from the terminal; overrides --debug-log-cfg")
-		config.BindPFlag("no_daemonize", pflag.Lookup("no-detach"))
+		config.BindPFlag("no_detach", pflag.Lookup("no-detach"))
 	}
 
 	// deprecated flags
@@ -121,7 +127,7 @@ func init() {
 	config.AutomaticEnv()
 }
 
-func NewConfig() (*Config, error) {
+func NewConfigFromEnv() (*Config, error) {
 	pflag.Parse()
 
 	c := &Config{}
@@ -137,54 +143,136 @@ func NewConfig() (*Config, error) {
 		config.Set("no_daemonize", true)
 	}
 
-	// unmarshal the viper config into our own struct
-	if err := config.Unmarshal(c); err != nil {
+	// unmarshal environment config into our Config object here
+	decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+		Result:           c,
+		WeaklyTypedInput: true,
+		DecodeHook:       decodeHook,
+	})
+	if err != nil {
 		return nil, err
 	}
 
-	// collect any extra args passed and add them to our file list
-	for f := range pflag.Args() {
-		c.Files = append(c.Files, interface{}(f))
+	if err = decoder.Decode(config.AllSettings()); err != nil {
+		return nil, err
+	}
+
+	// explicitly set destination protocol if we've asked for tcp or tls
+	if c.TLS {
+		c.Destination.Protocol = "tls"
+	}
+	if c.TCP {
+		c.Destination.Protocol = "tcp"
+	}
+
+	// add the papertrail root CA if necessary
+	if c.Destination.Protocol == "tls" && c.Destination.Host == "logs.papertrailapp.com" {
+		c.RootCAs = papertrail.RootCA()
+	}
+
+	// figure out where to create a pidfile if none was configured
+	if c.PidFile == "" {
+		c.PidFile = getPidFile()
+	}
+
+	// collect any extra args passed on the command line and add them to our file list
+	for _, file := range pflag.Args() {
+		files, err := decodeLogFiles([]interface{}{file})
+		if err != nil {
+			return nil, err
+		}
+
+		c.Files = append(c.Files, files...)
 	}
 
 	return c, nil
 }
 
-func (c *Config) Daemonize() bool {
-	return !c.NoDaemonize
-}
-
-func (c *Config) GetDestinationProtocol() string {
-	if c.TLS {
-		return "tls"
+func (c *Config) Validate() error {
+	if c.Destination.Host == "" {
+		return fmt.Errorf("No destination hostname specified")
 	}
 
-	if c.TCP {
-		return "tcp"
-	}
-
-	return c.Destination.Protocol
-}
-
-func (c *Config) RootCAs() *x509.CertPool {
-	if c.GetDestinationProtocol() == "tls" && c.Destination.Host == "logs.papertrailapp.com" {
-		return papertrail.RootCA()
+	if c.NewFileCheckInterval < 1*time.Second {
+		return fmt.Errorf("new_file_check_interval is too small, try setting >= 1")
 	}
 
 	return nil
 }
 
-func (c *Config) GetFiles() []LogFile {
-	var files []LogFile
-	for _, f := range c.Files {
-		switch val := f.(type) {
+func decodeDuration(f interface{}) (time.Duration, error) {
+	var (
+		i   int
+		err error
+	)
+
+	switch val := f.(type) {
+	case string:
+		i, err = strconv.Atoi(val)
+		if err != nil {
+			return 0, err
+		}
+
+	case int:
+		i = val
+
+	case time.Duration:
+		return val, nil
+
+	default:
+		return 0, fmt.Errorf("Invalid duration: %#v", val)
+	}
+
+	return time.Duration(i) * time.Second, nil
+}
+
+func decodeRegexps(f interface{}) ([]*regexp.Regexp, error) {
+	rs, ok := f.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("Invalid input type for regular expression %#v", f)
+	}
+
+	exps := make([]*regexp.Regexp, len(rs))
+	for i, r := range rs {
+		str, ok := r.(string)
+		if !ok {
+			return nil, fmt.Errorf("Invalid input type for regular expression %#v", r)
+		}
+
+		exp, err := regexp.Compile(str)
+		if err != nil {
+			return nil, err
+		}
+
+		exps[i] = exp
+	}
+
+	return exps, nil
+}
+
+func decodeLogFiles(f interface{}) ([]LogFile, error) {
+	var (
+		files []LogFile
+	)
+
+	vals, ok := f.([]interface{})
+	if !ok {
+		return files, fmt.Errorf("Invalid input type for files: %#v", f)
+	}
+
+	for _, v := range vals {
+		switch val := v.(type) {
 		case string:
 			lf := strings.Split(val, "=")
-			if len(lf) == 2 {
+			switch len(lf) {
+			case 2:
 				files = append(files, LogFile{Tag: lf[0], Path: lf[1]})
-			} else {
+			case 1:
 				files = append(files, LogFile{Path: val})
+			default:
+				return files, fmt.Errorf("Invalid log file name %s", val)
 			}
+
 		case map[interface{}]interface{}:
 			var (
 				tag  string
@@ -194,20 +282,56 @@ func (c *Config) GetFiles() []LogFile {
 			tag, _ = val["tag"].(string)
 			path, _ = val["path"].(string)
 
-			if path != "" {
-				files = append(files, LogFile{Tag: tag, Path: path})
+			if path == "" {
+				return files, fmt.Errorf("Invalid log file %#v", val)
 			}
+
+			files = append(files, LogFile{Tag: tag, Path: path})
+
+		default:
+			panic(vals)
 		}
 	}
 
-	return files
+	return files, nil
 }
 
-func (c *Config) GetPidFile() string {
-	if c.PidFile != "" {
-		return c.PidFile
+func decodePriority(p interface{}) (interface{}, error) {
+	ps, ok := p.(string)
+	if !ok {
+		return nil, fmt.Errorf("Invalid priority: %#v", p)
 	}
 
+	pri, err := syslog.Severity(ps)
+	if err == nil {
+		return pri, nil
+	}
+
+	// if it's not a severity, try facility
+	pri, err = syslog.Facility(ps)
+	if err == nil {
+		return pri, nil
+	}
+
+	return nil, fmt.Errorf("%s: %s", err.Error(), ps)
+}
+
+func decodeHook(from reflect.Type, to reflect.Type, data interface{}) (interface{}, error) {
+	switch to {
+	case reflect.TypeOf([]LogFile{}):
+		return decodeLogFiles(data)
+	case reflect.TypeOf([]*regexp.Regexp{}):
+		return decodeRegexps(data)
+	case reflect.TypeOf(syslog.Priority(0)):
+		return decodePriority(data)
+	case reflect.TypeOf(time.Duration(0)):
+		return decodeDuration(data)
+	}
+
+	return data, nil
+}
+
+func getPidFile() string {
 	pidFiles := []string{
 		"/var/run/remote_syslog.pid",
 		os.Getenv("HOME") + "/run/remote_syslog.pid",
--- config_test.go
+++ config_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"regexp"
 	"testing"
+	"time"
 
+	"github.com/papertrail/remote_syslog2/papertrail"
+	"github.com/papertrail/remote_syslog2/syslog"
 	"github.com/spf13/pflag"
 	"github.com/stretchr/testify/assert"
 )
@@ -14,7 +18,7 @@ func TestRawConfig(t *testing.T) {
 	pflag.Set("configfile", "example_config.yml")
 	pflag.Set("tls", "true")
 
-	c, err := NewConfig()
+	c, err := NewConfigFromEnv()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -22,43 +26,9 @@ func TestRawConfig(t *testing.T) {
 	assert.Equal(c.Destination.Host, "logs.papertrailapp.com")
 	assert.Equal(c.Destination.Port, 514)
 	assert.Equal(c.Destination.Protocol, "tls")
-	assert.Equal(c.ExcludePatterns, []string{"don't log on me", `do \w+ on me`})
-	assert.Equal(c.ExcludeFiles, []string{`\.DS_Store`})
-	assert.Equal(c.Files, []interface{}{
-		"locallog.txt",
-		"/var/log/**/*.log",
-		"nginx=/var/log/nginx/nginx.log",
-		map[interface{}]interface{}{"path": "/var/log/httpd/access_log", "tag": "apache"},
-	})
-	assert.Equal(c.TcpMaxLineLength, 99991)
-	assert.Equal(c.NewFileCheckInterval, 10)
-	assert.Equal(c.ConnectTimeout, 5)
-	assert.Equal(c.WriteTimeout, 0)
-	assert.Equal(c.TCP, false)
-	assert.Equal(c.TLS, true)
-	assert.Equal(c.LogLevels, "<root>=INFO")
-	assert.Equal(c.PidFile, "/var/run/rs2.pid")
-	assert.Equal(c.DebugLogFile, "/dev/null")
-	assert.Equal(c.NoDaemonize, false)
-	assert.Equal(c.Severity, "notice")
-	assert.Equal(c.Facility, "user")
-	assert.NotEqual(c.Hostname, "")
-	assert.Equal(c.Poll, false)
-}
-
-func TestComputedConfig(t *testing.T) {
-	assert := assert.New(t)
-
-	// pretend like some things were passed on the command line
-	pflag.Set("configfile", "example_config.yml")
-
-	c, err := NewConfig()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	assert.Equal("tls", c.GetDestinationProtocol())
-	assert.Equal([]LogFile{
+	assert.Equal(c.ExcludePatterns, []*regexp.Regexp{regexp.MustCompile("don't log on me"), regexp.MustCompile(`do \w+ on me`)})
+	assert.Equal(c.ExcludeFiles, []*regexp.Regexp{regexp.MustCompile(`\.DS_Store`)})
+	assert.Equal(c.Files, []LogFile{
 		{
 			Path: "locallog.txt",
 		},
@@ -73,5 +43,28 @@ func TestComputedConfig(t *testing.T) {
 			Tag:  "apache",
 			Path: "/var/log/httpd/access_log",
 		},
-	}, c.GetFiles())
+	})
+	assert.Equal(c.TcpMaxLineLength, 99991)
+	assert.Equal(c.NewFileCheckInterval, 10*time.Second)
+	assert.Equal(c.ConnectTimeout, 5*time.Second)
+	assert.Equal(c.WriteTimeout, 30*time.Second)
+	assert.Equal(c.TCP, false)
+	assert.Equal(c.TLS, true)
+	assert.Equal(c.LogLevels, "<root>=INFO")
+	assert.Equal(c.PidFile, "/var/run/rs2.pid")
+	assert.Equal(c.DebugLogFile, "/dev/null")
+	assert.Equal(c.NoDaemonize, false)
+	sev, err := syslog.Severity("notice")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(c.Severity, sev)
+	fac, err := syslog.Facility("user")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(c.Facility, fac)
+	assert.NotEqual(c.Hostname, "")
+	assert.Equal(c.Poll, false)
+	assert.Equal(c.RootCAs, papertrail.RootCA())
 }
